0326812cce114dccc91569e9beae383d1e1ba4fb
Fredrik Rofors <fredrik_rofors@hotmail.com>
2016-02-03 20:30:30 +0100

    Added BeersContext

---
+++ Mvc5.App/Data/BeersContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Mvc5.App.Data
+{
+    public class BeersContext : DbContext
+    {
+        public DbSet<Beer> Beers { get; set; }
+    }
+
+    public class Beer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
