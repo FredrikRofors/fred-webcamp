14782ce463189ca72d0ddbf911dd62f2a65f12f9
Fredrik Rofors <fredrik_rofors@hotmail.com>
2016-02-03 20:21:32 +0100

    Added out-of-the-box MVC5 app

---
+++ Mvc5.App/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Mvc5.App.Startup))]
+namespace Mvc5.App
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
